76dc42a9e28118aa16a028964f3a467f0351035b
jamesra <james.r.anderson@utah.edu>
2015-01-14 16:37:45 -0700

    Add change data tracking to database create/update scripts Use the more precise SYSUTCDATETIME for last modified trigger.

--- Servers/AnnotationService/SQL/DatabaseCreateUpdate/CreateUpdateDatabase.sql
+++ Servers/AnnotationService/SQL/DatabaseCreateUpdate/CreateUpdateDatabase.sql
@@ -1961,6 +1961,155 @@ end
 	 COMMIT TRANSACTION sixteen
 	end
 
+	if(not(exists(select (1) from DBVersion where DBVersionID = 17)))
+	begin
+     print N'Create role for change logging'
+     BEGIN TRANSACTION seventeen
+		 
+		EXEC('CREATE ROLE ChangeTracker');
+				
+		--any potential errors get reported, and the script is rolled back and terminated
+		 if(@@error <> 0)
+		 begin
+		   ROLLBACK TRANSACTION 
+		   RETURN
+		 end
+
+		  --insert the second version marker
+		 INSERT INTO DBVersion values (17, 
+		   'Create role for change logging',getDate(),User_ID())
+
+	 COMMIT TRANSACTION seventeen
+	end
+
+	
+	if(not(exists(select (1) from DBVersion where DBVersionID = 18)))
+	begin
+     print N'Enable change logging'
+     BEGIN TRANSACTION eighteen
+		 
+		 ALTER AUTHORIZATION ON DATABASE::{DATABASE_NAME} TO Sa;
+		 if(@@error <> 0)
+		 begin
+		   ROLLBACK TRANSACTION 
+		   RETURN
+		 end
+
+		 EXEC sys.sp_cdc_enable_db
+				
+		--any potential errors get reported, and the script is rolled back and terminated
+		 if(@@error <> 0)
+		 begin
+		   ROLLBACK TRANSACTION 
+		   RETURN
+		 end
+
+		 EXEC sys.sp_cdc_enable_table @source_schema = N'dbo',
+								      @source_name = N'Location',
+									  @role_name = N'ChangeTracker',
+									  @supports_net_changes = 1
+		if(@@error <> 0)
+		 begin
+		   ROLLBACK TRANSACTION 
+		   RETURN
+		 end
+
+		 EXEC sys.sp_cdc_enable_table @source_schema = N'dbo',
+								      @source_name = N'Structure',
+									  @role_name = N'ChangeTracker',
+									  @supports_net_changes = 1
+		if(@@error <> 0)
+		 begin
+		   ROLLBACK TRANSACTION 
+		   RETURN
+		 end
+
+		 EXEC sys.sp_cdc_enable_table @source_schema = N'dbo',
+								      @source_name = N'StructureType',
+									  @role_name = N'ChangeTracker',
+									  @supports_net_changes = 1
+		if(@@error <> 0)
+		 begin
+		   ROLLBACK TRANSACTION 
+		   RETURN
+		 end
+
+		 EXEC sys.sp_cdc_enable_table @source_schema = N'dbo',
+								      @source_name = N'StructureLink',
+									  @role_name = N'ChangeTracker'
+		if(@@error <> 0)
+		 begin
+		   ROLLBACK TRANSACTION 
+		   RETURN
+		 end
+
+		 EXEC sys.sp_cdc_enable_table @source_schema = N'dbo',
+								      @source_name = N'LocationLink',
+									  @role_name = N'ChangeTracker',
+									  @supports_net_changes = 1
+				
+		--any potential errors get reported, and the script is rolled back and terminated
+		 if(@@error <> 0)
+		 begin
+		   ROLLBACK TRANSACTION 
+		   RETURN
+		 end
+
+		  --insert the second version marker
+		 INSERT INTO DBVersion values (18, 
+		   'Enable change logging',getDate(),User_ID())
+
+	 COMMIT TRANSACTION eighteen
+	end
+	
+	if(not(exists(select (1) from DBVersion where DBVersionID = 19)))
+	begin
+     print N'Update triggers to use more precise SYSUTCDATETIME() function'
+     BEGIN TRANSACTION nineteen
+		 
+		EXEC('ALTER TRIGGER [dbo].[Location_update] 
+				ON  [dbo].[Location]
+				FOR UPDATE
+				AS 
+					Update dbo.Location
+					Set LastModified = (SYSUTCDATETIME())
+					WHERE ID in (SELECT ID FROM inserted)
+					-- SET NOCOUNT ON added to prevent extra result sets from
+					-- interfering with SELECT statements.
+					SET NOCOUNT ON;');
+				
+		--any potential errors get reported, and the script is rolled back and terminated
+		 if(@@error <> 0)
+		 begin
+		   ROLLBACK TRANSACTION 
+		   RETURN
+		 end
+
+		 EXEC('ALTER TRIGGER [dbo].[Structure_LastModified] 
+			   ON  [dbo].[Structure]
+			   FOR UPDATE
+			AS 
+				Update dbo.[Structure]
+				Set LastModified = (SYSUTCDATETIME())
+				WHERE ID in (SELECT ID FROM inserted)
+				-- SET NOCOUNT ON added to prevent extra result sets from
+				-- interfering with SELECT statements.
+				SET NOCOUNT ON;');
+				
+		--any potential errors get reported, and the script is rolled back and terminated
+		 if(@@error <> 0)
+		 begin
+		   ROLLBACK TRANSACTION 
+		   RETURN
+		 end
+
+		  --insert the second version marker
+		 INSERT INTO DBVersion values (19, 
+		   'Update triggers to use more precise SYSUTCDATETIME() function',getDate(),User_ID())
+
+	 COMMIT TRANSACTION nineteen
+	end
+
 --from here on, continually add steps in the previous manner as needed.
 	COMMIT TRANSACTION main
  
