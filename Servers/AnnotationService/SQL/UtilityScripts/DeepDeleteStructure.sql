dd86ae4504263575b1fcd617fc41e81595e67033
jamesra <james.r.anderson@utah.edu>
2015-03-19 13:08:42 -0600

    Extended, still needs work to be recursive

--- Servers/AnnotationService/SQL/UtilityScripts/DeepDeleteStructure.sql
+++ Servers/AnnotationService/SQL/UtilityScripts/DeepDeleteStructure.sql
@@ -1,5 +1,5 @@
 DECLARE @DeleteID bigint
-Set @DeleteID = 449
+Set @DeleteID = 268
 
 delete from LocationLink
 where A in 
@@ -13,8 +13,20 @@ where B in
 Select ID from Location 
 where ParentID = @DeleteID ) 
 
+delete from StructureLink
+where SourceID in 
+(select ID from Structure where ParentID = @DeleteID)
+
+delete from StructureLink
+where TargetID in 
+(select ID from Structure where ParentID = @DeleteID)
+
 delete from Location
 where ParentID=@DeleteID
 
 delete from Structure
+where ParentID = @DeleteID
+
+delete from Structure
 where ID=@DeleteID
+  
